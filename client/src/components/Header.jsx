98d75e03980bcdf835618554ff491c13adb8fbab
samshawon10 <shawonakando518@gmail.com>
2026-03-19 16:54:33 +0600

    shoevista test

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,12 +13,12 @@ const Header = () => {
     const toggleMenu = () => setIsOpen(prev => !prev);
 
     return (
-        <nav className='sticky top-0 z-40 text-sm bg-white flex flex-col md:flex-row md:items-center md:justify-between p-4 py-2 gap-2 shadow-sm'>
-            <div className='flex justify-between items-center'>
+        <nav className='sticky top-0 z-40 flex flex-col gap-2 p-4 py-2 text-sm bg-white shadow-sm md:flex-row md:items-center md:justify-between'>
+            <div className='flex items-center justify-between'>
                 {/* Logo */}
                 <Link
                     to="/"
-                    className="logo text-xl font-bold"
+                    className="text-xl font-bold logo"
                     onClick={toggleMenu}
                 >
                     ShoeVista
@@ -27,7 +27,7 @@ const Header = () => {
                 {/* Toggle Button for Mobile */}
                 <button
                     onClick={toggleMenu}
-                    className="md:hidden flex items-center"
+                    className="flex items-center md:hidden"
                 >
                     {
                         isOpen ? (
@@ -48,10 +48,10 @@ const Header = () => {
             <div className={`flex flex-col md:flex-row md:items-center ${isOpen ? 'block' : 'hidden md:flex'}`}>
                 {/* Links */}
                 <ul className='flex flex-col md:flex-row md:gap-4 md:pr-6'>
-                    {['/', '/shoes/men', '/shoes/women', '/shoes/kids'].map((path, index) => {
-                        const labels = ['Home', 'Men', 'Women', 'Kids'];
+                    {['/', '/shoes/men', '/shoes/women', '/shoes/kids','/about-us'].map((path, index) => {
+                        const labels = ['Home', 'Men', 'Women', 'Kids','About Us'];
                         return (
-                            <li key={path} className='hover:bg-slate-100 transition-all duration-300 delay-100 p-2 rounded'>
+                            <li key={path} className='p-2 transition-all duration-300 delay-100 rounded hover:bg-slate-100'>
                                 <NavLink
                                     to={path}
                                     onClick={toggleMenu}
@@ -73,7 +73,7 @@ const Header = () => {
                             onClick={toggleMenu}
                         >
                             <div className='relative'>
-                                <span className='w-4 text-center rounded-xl absolute bottom-4 left-4 text-xs bg-gray-900 text-white'>
+                                <span className='absolute w-4 text-xs text-center text-white bg-gray-900 rounded-xl bottom-4 left-4'>
                                     {list.length > 0 ? list.length : ''}
                                 </span>
                                 <img src="/Navbar/wishlist.png" alt="wishlist" className='w-6 h-6 hover:scale-110' />
@@ -84,7 +84,7 @@ const Header = () => {
                             onClick={toggleMenu}
                         >
                             <div className='relative'>
-                                <span className='w-4 text-center rounded-xl absolute bottom-4 left-4 text-xs bg-gray-900 text-white'>
+                                <span className='absolute w-4 text-xs text-center text-white bg-gray-900 rounded-xl bottom-4 left-4'>
                                     {cartList.length > 0 ? cartList.length : ''}
                                 </span>
                                 <img src="/Navbar/cart.png" alt="cart" className='w-6 h-6 hover:scale-110' />
